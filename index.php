d047cc66cf42adeaafe7b01fd62b78a64b09151a
bruna <bruna.ssantos102@senacsp.edu.br>
2023-11-16 17:10:44 -0300

    Slide index

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
+?> 
+
+
+
+    <main class="container mt-3 mb-3 text-center">
+        <h1>HOME</h1>
+
+        <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/assets/img/logomarca.png" class="d-block w-60" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/assets/img/images.jpg" class="d-block w-60" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/assets/img/recarga-de-credito.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </main>
+
+
+
+
+
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/rodape.php";?> 
+
